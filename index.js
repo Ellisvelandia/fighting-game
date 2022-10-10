256ff281fbeeb3d5ab86ea68cba03be5bb6ef1f0
ellisvelandia <eyis619@gmail.com>
2022-10-09 21:22:32 -0500

    health

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100;
   }
   draw() {
     c.fillStyle = this.color;
@@ -162,7 +163,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
-    console.log("go");
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
   if (
@@ -173,7 +175,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
-    console.log("enemy attack successful");
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 }
 
